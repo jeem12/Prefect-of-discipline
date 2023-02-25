94f3055840913aefbe6c39dd652450013c538757
jeem12 <zernajm2000@gmail.com>
2023-02-25 17:00:39 +0800

    Update check_login.php

--- assets/php/check_login.php
+++ assets/php/check_login.php
@@ -13,7 +13,16 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 	}
 
 	$uname = validate($_POST['uname']);
-	$pass = md5(($_POST['password']));
+	$pass = validate($_POST['password']);
+
+	$ciphering = "aes-128-ctr";
+    $option = 0;
+    $decryption_iv = '1234567890123456';
+    $decryption_key = 'devanshu';
+    $decryption = openssl_decrypt($pass, $ciphering, $decryption_key, $option, $decryption_iv);
+
+
+	
 
 	if (empty($uname)) {
 		header("Location: ../../index.php?error=User Name is required");
@@ -22,24 +31,24 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
         header("Location: ../../index.php?error=Password is required");
 	    exit();
 	}else{
-		$sql = "SELECT * FROM podms_users WHERE username='$uname' AND password='$pass'";
+		$sql = "SELECT * FROM podms_users WHERE username='$uname' AND password='$decryption'";
 
 		$result = mysqli_query($conn, $sql);
 
 		if (mysqli_num_rows($result) === 1) {
 			$row = mysqli_fetch_assoc($result);
-            if ($row['username'] === $uname && $row['password'] === $pass) {
+            if ($row['username'] === $uname && $row['password'] === $decryption) {
             	$_SESSION['user_name'] = $row['username'];
             	$_SESSION['name'] = $row['name'];
             	$_SESSION['id'] = $row['id'];
             	header("Location: ../../subsystem_folder/index.php");
 		        exit();
             }else{
-				header("Location: ../../index.php?error=Incorect User name or password");
+				header("Location: ../../index.php?error=Incorrect User name or password");
 		        exit();
 			}
 		}else{
-			header("Location: ../../index.php?error=Incorect User name or password");
+			header("Location: ../../index.php?error=Incorrect User name or password");
 	        exit();
 		}
 	}
